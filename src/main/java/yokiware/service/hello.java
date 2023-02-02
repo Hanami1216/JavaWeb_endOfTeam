01e720986afe2820437b24fd5e991124d930708d
PC-Princess <yoki001216@gmail.com>
2023-02-02 15:46:09 +0800

    servlet框架导入

--- src/main/java/yokiware/service/hello.java
+++ src/main/java/yokiware/service/hello.java
@@ -1,12 +1,13 @@
 package yokiware.service;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
+@WebServlet("/hello")
 public class hello extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
