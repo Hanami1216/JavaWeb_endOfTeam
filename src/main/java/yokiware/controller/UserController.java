ca9d14668c0ee9dcff34b8af020751157ec8155a
PC-Princess <yoki001216@gmail.com>
2023-02-19 13:14:07 +0800

    User GET请求完善

--- src/main/java/yokiware/controller/UserController.java
+++ src/main/java/yokiware/controller/UserController.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -19,7 +20,7 @@ import java.util.List;
  * @Author : YokiWare
  * @Date: 2023/2/5  14:12
  */
-@WebServlet("/user")
+@WebServlet("/user/*")
 public class UserController extends HttpServlet {
     private static final Logger logger = Logger.getLogger(UserController.class);
     private final UserService userService = new UserServiceImpl();
@@ -28,28 +29,31 @@ public class UserController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        user = userService.getById(Integer.parseInt(req.getParameter("id")));
+
 
 
         String requestURI = req.getRequestURI();
         String[] uriParts = requestURI.split("/");
+        System.out.println(Arrays.toString(uriParts));
 
         // 获取所有用户信息
-        if (uriParts.length == 2 || (uriParts.length == 3 && uriParts[2].isEmpty())) {
+        if (uriParts.length == 3 || (uriParts.length == 4 && uriParts[3].isEmpty())) {
             userList = userService.getAll();
 //            String usersJson = new Gson().toJson(users); // 将 Java 对象转换成 JSON 格式的字符串
 //            response.setContentType("application/json"); // 将响应的内容类型设置为 JSON
 //            response.getWriter().write(usersJson);
             if (userList != null) {
-                JSONUtil.responseOutWithJson(resp, new Result(Code.GET_OK, "GET成功,返回所有用户信息", user));
+                JSONUtil.responseOutWithJson(resp, new Result(Code.GET_OK, "GET成功,返回所有用户信息", userList));
             } else
                 JSONUtil.responseOutWithJson(resp, new Result(Code.GET_ERR, "GET失败，返回null", null));
         }
         // 获取单个用户信息
-        else if (uriParts.length == 3) {
-            int userId = Integer.parseInt(uriParts[2]);
+        else if (uriParts.length == 4) {
+
+            int userId = Integer.parseInt(uriParts[3]);
+
             user = userService.getById(userId);
-            if (userList != null) {
+            if (user != null) {
                 JSONUtil.responseOutWithJson(resp, new Result(Code.GET_OK, "GET成功,返回单个用户信息", user));
             } else
                 JSONUtil.responseOutWithJson(resp, new Result(Code.GET_ERR, "GET失败，返回null", null));
